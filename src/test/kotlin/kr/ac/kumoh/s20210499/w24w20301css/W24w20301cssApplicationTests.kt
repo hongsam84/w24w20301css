a6ec7e142b4a52e4fa7f2f45d6c3b90523ffc747
DESKTOP-67IRD67\박준형 <pjh6395673066@gmail.com>
2024-09-25 13:37:58 +0900

    🎉 시작

---
+++ src/test/kotlin/kr/ac/kumoh/s20210499/w24w20301css/W24w20301cssApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210499.w24w20301css
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w20301cssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
